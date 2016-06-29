f29aca0b789a6bcc2e0975ae11763e5bc5d32ec1
ran.wei@york.ac.uk <ran.wei@york.ac.uk>
2016-06-29 14:55:28 +0100

    refactoring for AST2ExL transformations

--- org.eclipse.epsilon.haetae.dt/src/org/eclipse/epsilon/haetae/ast2exl/AST2ETLAction.java
+++ org.eclipse.epsilon.haetae.dt/src/org/eclipse/epsilon/haetae/ast2exl/AST2ETLAction.java
@@ -1,106 +1,21 @@
 package org.eclipse.epsilon.haetae.ast2exl;
 
-import java.io.File;
-import java.io.IOException;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.epsilon.eol.metamodel.EOLElement;
-import org.eclipse.epsilon.etl.EtlModule;
-import org.eclipse.epsilon.etl.ast2etl.Ast2EtlContext;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
+public class AST2ETLAction extends AbstractAST2ExLAction {
 
-public class AST2ETLAction implements IObjectActionDelegate {
+	@Override
+	protected String getExtension() {
+		return "etl";
+	}
 
-	private Shell shell;
+	@Override
+	protected String getOperationType() {
+		return "AST2ETL";
+	}
+
+	@Override
+	protected String getExtensionPrefix() {
+		return "";
+	}
 	
-	public AST2ETLAction() {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void run(IAction action) {
-
-		IWorkbenchWindow window =
-			    PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-			ISelection selection = window.getSelectionService().getSelection("org.eclipse.jdt.ui.PackageExplorer");
-
-		
-		IFile file = (IFile) ((IStructuredSelection) selection).getFirstElement();
-
-		IPath path = file.getLocation();
-		
-		if (!path.getFileExtension().equals("etl")) {
-			MessageDialog dialog = new MessageDialog(shell, "Haetae goes woof", null,
-				    "AST2EOL converter only works for .etl files", MessageDialog.ERROR, new String[] { "OK" }, 0);
-				dialog.open();
-		}
-		else {
-			EtlModule etlModulel = new EtlModule();
-			
-			try {
-				etlModulel.parse(new File(path.toPortableString()));
-			} catch (Exception e) {
-				e.printStackTrace();
-				MessageDialog dialog = new MessageDialog(shell, "Haetae goes woof", null,
-					    "Unable to parse file, please ensure the .etl file does not contain syntax errors", MessageDialog.ERROR, new String[] { "OK" }, 0);
-					dialog.open();
-			}
-
-			Ast2EtlContext context = new Ast2EtlContext(etlModulel);
-			EOLElement evlElement = null;
-			try {
-				evlElement = (EOLElement) context.getEtlElementCreatorFactory().createEOLElement(etlModulel.getAst(), null, context);
-			} catch (Exception e) {
-				e.printStackTrace();
-				MessageDialog dialog = new MessageDialog(shell, "Haetae goes woof", null,
-					    "Unable to parse file, please ensure the .etl file does not contain syntax errors", MessageDialog.ERROR, new String[] { "OK" }, 0);
-					dialog.open();
-			}
-			
-			ResourceSet resourceSet = new ResourceSetImpl();
-			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
-			Resource resource = resourceSet.createResource(URI.createFileURI(new File(path.toPortableString() + ".model").getAbsolutePath()));
-			resource.getContents().add(evlElement);
-			try {
-				resource.save(null);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				MessageDialog dialog = new MessageDialog(shell, "Haetae goes woof", null,
-					    "Unable to save file", MessageDialog.ERROR, new String[] { "OK" }, 0);
-					dialog.open();
-			}
-			
-			MessageDialog dialog = new MessageDialog(shell, "Haetae says:", null,
-				    "Transformation completed, " + path.toFile().getName() + ".model saved to directory", MessageDialog.INFORMATION, new String[] { "OK" }, 0);
-				dialog.open();
-		}
-	}
-
-	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
--- org.eclipse.epsilon.haetae.dt/src/org/eclipse/epsilon/haetae/ast2exl/AST2EVLAction.java
+++ org.eclipse.epsilon.haetae.dt/src/org/eclipse/epsilon/haetae/ast2exl/AST2EVLAction.java
@@ -1,106 +1,21 @@
 package org.eclipse.epsilon.haetae.ast2exl;
 
-import java.io.File;
-import java.io.IOException;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.epsilon.eol.metamodel.EOLElement;
-import org.eclipse.epsilon.evl.EvlModule;
-import org.eclipse.epsilon.evl.ast2evl.Ast2EvlContext;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
+public class AST2EVLAction extends AbstractAST2ExLAction{
 
-public class AST2EVLAction implements IObjectActionDelegate{
-
-	private Shell shell;
-
-	public AST2EVLAction() {
-		// TODO Auto-generated constructor stub
+	@Override
+	protected String getExtension() {
+		return "evl";
 	}
 
 	@Override
-	public void run(IAction action) {
-
-		IWorkbenchWindow window =
-			    PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-			ISelection selection = window.getSelectionService().getSelection("org.eclipse.jdt.ui.PackageExplorer");
-
-		
-		IFile file = (IFile) ((IStructuredSelection) selection).getFirstElement();
-
-		IPath path = file.getLocation();
-		
-		if (!path.getFileExtension().equals("evl")) {
-			MessageDialog dialog = new MessageDialog(shell, "Haetae goes woof", null,
-				    "AST2EOL converter only works for .evl files", MessageDialog.ERROR, new String[] { "OK" }, 0);
-				dialog.open();
-		}
-		else {
-			EvlModule evlModule = new EvlModule();
-			
-			try {
-				evlModule.parse(new File(path.toPortableString()));
-			} catch (Exception e) {
-				e.printStackTrace();
-				MessageDialog dialog = new MessageDialog(shell, "Haetae goes woof", null,
-					    "Unable to parse file, please ensure the .evl file does not contain syntax errors", MessageDialog.ERROR, new String[] { "OK" }, 0);
-					dialog.open();
-			}
-
-			Ast2EvlContext context = new Ast2EvlContext(evlModule);
-			EOLElement evlElement = null;
-			try {
-				evlElement = (EOLElement) context.getEvlElementCreatorFactory().createEOLElement(evlModule.getAst(), null, context);
-			} catch (Exception e) {
-				e.printStackTrace();
-				MessageDialog dialog = new MessageDialog(shell, "Haetae goes woof", null,
-					    "Unable to parse file, please ensure the .evl file does not contain syntax errors", MessageDialog.ERROR, new String[] { "OK" }, 0);
-					dialog.open();
-			}
-			
-			ResourceSet resourceSet = new ResourceSetImpl();
-			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
-			Resource resource = resourceSet.createResource(URI.createFileURI(new File(path.toPortableString() + ".model").getAbsolutePath()));
-			resource.getContents().add(evlElement);
-			try {
-				resource.save(null);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				MessageDialog dialog = new MessageDialog(shell, "Haetae goes woof", null,
-					    "Unable to save file", MessageDialog.ERROR, new String[] { "OK" }, 0);
-					dialog.open();
-			}
-			
-			MessageDialog dialog = new MessageDialog(shell, "Haetae says:", null,
-				    "Transformation completed, " + path.toFile().getName() + ".model saved to directory", MessageDialog.INFORMATION, new String[] { "OK" }, 0);
-				dialog.open();
-		}
+	protected String getOperationType() {
+		return "AST2EVL";
 	}
 
 	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-		
+	protected String getExtensionPrefix() {
+		return "";
 	}
-
-	@Override
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
